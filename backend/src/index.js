76c10e32c6b6c20a4101aa966609654b99c69db9
leoriq <30530075+leoriq@users.noreply.github.com>
2021-01-15 18:55:00 -0300

    BD, Done e cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const cors = require('cors');
+
 const app = express();
+
+const server = require('http').Server(app);
 
 mongoose.connect('mongodb+srv://unectproj2:unectproj2@cluster0.xi8yy.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useNewUrlParser: true,
 })
 
-app.listen(3333);
+app.use(cors());
+
+server.listen(3333);
