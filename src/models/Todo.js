6b9deab6e4fd739e40af3845c90c82f5ff5ef720
leoriq <30530075+leoriq@users.noreply.github.com>
2021-01-21 20:30:17 -0300

    Arrumacao 2?

--- src/models/Todo.js
+++ src/models/Todo.js
@@ -4,11 +4,7 @@ const TodoSchema = new mongoose.Schema({
     todo: String,
     done: {
         type: Boolean,
-<<<<<<< HEAD
         default: false,
-=======
-        default: false
->>>>>>> 9df313471338bf86cfd23a9f6b81724af42eec60
     },
 }, {
     timestamps: true,
