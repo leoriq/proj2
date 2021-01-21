c18aabe9cc32d2e7754510ff78290772f7e7bd2f
leoriq <30530075+leoriq@users.noreply.github.com>
2021-01-21 20:07:50 -0300

    Arrumacao para front

--- src/controllers/DoneController.js
+++ src/controllers/DoneController.js
@@ -2,7 +2,6 @@ const Todo = require('../models/Todo');
 
 module.exports = {
     async store(req, res) {
-<<<<<<< HEAD
         const todo = await Todo.findById(req.params.id);
 
         todo.done = true;
@@ -12,14 +11,5 @@ module.exports = {
         req.io.emit('done', todo);
 
         return res.json(todo);
-=======
-        const todo = await Todo.findById(req.name.id);
-
-        todo.done == true;
-
-        await todo.save();
-
-        return res.json(post);
->>>>>>> 9df313471338bf86cfd23a9f6b81724af42eec60
     }
 }
